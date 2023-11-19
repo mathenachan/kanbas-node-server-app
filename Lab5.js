43d601327c02278a799ed969d21a8dc06eea47f3
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-11-19 18:24:35 -0500

    completed lab

--- Lab5.js
+++ Lab5.js
@@ -27,6 +27,25 @@ const Lab5 = (app) => {
         }
         res.json(todos);
     });
+    app.put("/a5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todo.title = req.body.title;
+        todo.description = req.body.description;
+        todo.due = req.body.due;
+        todo.completed = req.body.completed;
+        res.sendStatus(200);
+      });
+    
+    app.post("/a5/todos", (req, res) => {
+        const newTodo = {
+          ...req.body,
+          id: new Date().getTime(),
+        };
+        todos.push(newTodo);
+        res.json(newTodo);
+      });
+    
     app.get("/a5/todos/create", (req, res) => {
         const newTodo = {
             id: new Date().getTime(),
@@ -36,6 +55,13 @@ const Lab5 = (app) => {
         todos.push(newTodo);
         res.json(todos);
     });
+    app.delete("/a5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todos.splice(todos.indexOf(todo), 1);
+        res.sendStatus(200);
+      });
+    
     app.get("/a5/todos/:id/delete", (req, res) => {
         const { id } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
