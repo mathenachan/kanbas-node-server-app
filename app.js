884709adf0e91e787ef0e1e894d0903579e5c36c
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-11-20 02:41:22 -0500

    add env variables

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ import Lab5 from './Lab5.js';
 import CourseRoutes from './courses/routes.js';
 import cors from "cors";
 import ModuleRoutes from './modules/routes.js';
+import "dotenv/config"
 
-const app = express()
+const app = express();
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
-Hello(app)
-Lab5(app)
-app.listen(4000)
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
