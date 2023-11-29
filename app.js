58bf8e13a53766f4b09d266c5ab98c63d362dd99
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-11-29 01:49:08 -0500

    3.51

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ import CourseRoutes from './courses/routes.js';
 import cors from "cors";
 import mongoose from "mongoose";
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import UserRoutes from './users/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import "dotenv/config"
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Hello(app);
